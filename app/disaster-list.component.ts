737bcdd35e27091cf32b36cb8a3edc68bed689d6
Sandeep-Vanjari <73571862+Sandeep-Vanjari@users.noreply.github.com>
2020-11-03 14:22:14 +0530

    Add files via upload

---
+++ app/disaster-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-disaster-list',
+  templateUrl: './disaster-list.component.html',
+  styleUrls: ['./disaster-list.component.css']
+})
+export class DisasterListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
